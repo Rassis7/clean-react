f0b18c161f8331d6e701141bf6f4dd2500fa3d5b
Romulo Assis <romuloassis.as@gmail.com>
2021-02-18 23:16:13 -0300

    refactor: mova axios mock to a separator file

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { HttpPostParams, HttpPostClient, HttpResponse } from '@/data/protocols/http';
 
+// Adapter
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post ({ url, body }: HttpPostParams<any>): Promise<HttpResponse<any>> {
     const { status, data } = await axios.post(url, body)
