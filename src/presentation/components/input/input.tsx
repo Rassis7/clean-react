1595e80064e0bb946801141bb63903f5903208cd
Romulo Assis <romuloassis.as@gmail.com>
2021-02-21 17:40:07 -0300

    refactor: add index files with multiple imports

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -3,13 +3,9 @@ import Styles from './input-styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-const Input: React.FC<Props> = (props: Props) => {
-  return (
-    <div className={Styles.inputWrap}>
-      <input {...props}/>
-      <span className={Styles.status}>🔴</span>
-    </div>
-  )
-}
+const Input: React.FC<Props> = (props: Props) => <div className={Styles.inputWrap}>
+  <input {...props}/>
+  <span className={Styles.status}>🔴</span>
+</div>
 
 export default Input;
