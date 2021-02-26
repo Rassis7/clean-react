74918b746121a37d7d5b4de915639d02824d729d
Romulo Assis <romuloassis.as@gmail.com>
2021-02-25 21:20:30 -0300

    feat: ensure login calls validation with correct email

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,11 +4,11 @@ import Context from '@/presentation/contexts/form-context';
 import Styles from './form-status-styles.scss'
 
 const FormStatus: React.FC = () => {
-  const { state: { isLoading }, errorState: { main } } = useContext(Context)
+  const { state: { isLoading, mainError } } = useContext(Context)
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       {isLoading && <Spinner className={Styles.spinner}/>}
-      {main && <span className={Styles.error}>{main}</span>}
+      {mainError && <span className={Styles.error}>{mainError}</span>}
     </div>
   )
 }
--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,17 +5,20 @@ import Context from '@/presentation/contexts/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const enableInput = ({ target }: React.FocusEvent<HTMLInputElement>): void => {
     target.readOnly = false
+  }
+  const handleChange = ({ target }: React.FocusEvent<HTMLInputElement>): void => {
+    setState({ ...state, [target.name]: target.value })
   }
   const getStatus = (): string => '🔴'
   const getTitle = (): string => error;
 
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput}/>
+      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange}/>
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
