e70026ce34ee98b1801d4df3a3c0dede7909f663
Romulo Assis <romuloassis.as@gmail.com>
2021-02-28 15:48:41 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,18 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors';
 import { EmailValidation } from './email-validation';
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
-    expect(error).toEqual(new InvalidFieldError('email'))
+    const field = faker.random.word()
+    const sut = new EmailValidation(field)
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldError(field))
+  });
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   });
 });
