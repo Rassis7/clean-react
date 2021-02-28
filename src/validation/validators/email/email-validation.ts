c6937828a3010d77d1f209cf63f3e5e9dd6fad3c
Romulo Assis <romuloassis.as@gmail.com>
2021-02-28 15:48:30 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -7,6 +7,7 @@ export class EmailValidation implements FieldValidation {
   }
 
   validate (value: string): Error {
-    return new InvalidFieldError(this.field)
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(value) ? null : new InvalidFieldError(this.field)
   }
 }
