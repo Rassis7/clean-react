512868b19d69f705e7051ae272d11de7a5f5e9c5
Romulo Assis <romuloassis.as@gmail.com>
2021-03-02 23:44:47 -0300

    test: ensure MinLegth return false if value is vald

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,4 +7,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError('field'))
   });
+
+  it('Should return false if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  });
 });
