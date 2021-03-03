3a3d2644d5870c88909d94b227209bd6b78b02b2
Romulo Assis <romuloassis.as@gmail.com>
2021-03-02 23:41:16 -0300

    test: ensure MinLegth return error if value is invald

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,11 @@
+import { MinLengthValidation } from './min-length-validation';
+import { InvalidFieldError } from '@/validation/errors';
+import { FieldValidation } from '@/validation/protocols';
+
+describe('MinLengthValidation', () => {
+  it('Should return error if value is invalid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('123')
+    expect(error).toEqual(new InvalidFieldError('field'))
+  });
+});
